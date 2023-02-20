76ca7756d0a080f4aeb726a236f36e9e6fcdb569
pc <pc@DESKTOP-7NSEKGR>
2023-02-20 09:19:09 -0600

    Agregar archivos de proyecto.

---
+++ Personas/Contacto.cs
@@ -0,0 +1,30 @@
+﻿namespace Personas
+{
+    internal class Contacto : Persona
+    {
+        private string telefono;
+
+        public string Telefono
+        {
+            get { return telefono; }
+            set
+            {
+                telefono = value.Replace(" ", "").Replace("-", "");
+            }
+        }
+
+        public Contacto() : base()
+        {
+            telefono = string.Empty;
+        }
+        public Contacto(string nombre, DateTime? fechaNacimiento, string telefono) : base(nombre, fechaNacimiento)
+        {
+            this.telefono = telefono;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + " " + telefono;
+        }
+    }
+}
---
+++ Personas/Program.cs
@@ -0,0 +1,25 @@
+﻿using Personas;
+
+
+Console.WriteLine("¿Cuantos contactos desea agregar?");
+int N = Int32.Parse(Console.ReadLine());
+Contacto[] p = new Contacto[N];
+
+for (int i = 0; i < N; i++)
+{
+    Console.Clear();
+    p[i] = new Contacto();
+    Console.WriteLine("Contacto {0}\nNombre", i + 1);
+    p[i].Nombre = Console.ReadLine();
+    Console.WriteLine("Telefono");
+    p[i].Telefono = Console.ReadLine();
+    p[i].FechaNacimiento = DateTime.Parse("2003/01/01");
+}
+
+Console.Clear();
+
+for (int i = 0; i < N; i++)
+{
+    Console.WriteLine(p[i].ToString());
+}
+Console.ReadKey();
